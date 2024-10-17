5a7452d950baaea6a6bf2aea672924371cb0d49c
sunilverma11 <srksaroya@hotmail.com>
2024-10-17 18:47:47 +0530

    modified api's

--- lib/utils/jwt.ts
+++ lib/utils/jwt.ts
@@ -1,14 +1,41 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-// import jsonwebtoken from "jsonwebtoken"
+interface JwtPayloadExtended extends JwtPayload {
+  _id: string;
+  name:string;
+  error:string;
+}
+const privateKey = process.env.TOKEN_KEY || "";
+// const PRIVATE_KEY = process.env.TOKEN_KEY?.replace(/\\n/g, '\n') || '';
+console.log("private key ", privateKey)
+
 type IBody = {
     _id: string,
-    name: string
+    name: string,
+    error:string
 }
-export const generateToken = (body: IBody) => {
-    console.log("body in generate")
-    return body._id.toString();
+export const generateToken = async (body: IBody) => {
+    try {
+        console.log("body in generate")
+        const {name,_id}= body;
+        // const token = await jwt.sign({ _id: body._id }, privateKey, { algorithm: 'RS256' });
+        const token = await jwt.sign({ name,_id}, privateKey);
+        console.log("new token", token)
+        return token;
+    } catch (error) {
+        console.log("errrrrr", error)
+    }
+
 }
-export const compareToken = (token: string) => {
-    console.log("token in compare", token)
-    return true;
+export const verifyToken = (token: string):JwtPayloadExtended => {
+    try {
+        // console.log("token in verify", token)
+        const decoded = jwt.verify(token, privateKey) as JwtPayloadExtended;
+        // console.log("verify done")
+        console.log(decoded) // 
+        return decoded;
+    } catch (error) {
+        console.log(error);
+        return {error: 'Invalid token'} as JwtPayloadExtended
+    }
 }
--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -3,7 +3,7 @@ import { UserModel } from "@/lib/models/user.model";
 // import encryptPassword from "@/lib/middleware/auth"
 import dbConnect from "@/lib/dbConnect";
 import { comparePassword } from "@/lib/utils/bcrypt";
-import { generateToken } from "@/lib/utils/jwt";
+import { generateToken, verifyToken } from "@/lib/utils/jwt";
 
 
 export default async function UserController(req: NextApiRequest, res: NextApiResponse) {
@@ -18,10 +18,20 @@ export default async function UserController(req: NextApiRequest, res: NextApiRe
         case "GET":
             try {
                 //For Dashboard
-                const users = await UserModel.find({});
-                res.status(200).json(users);
+                //verify token
+                const token = req.headers.authorization || ""
+                console.log("in req get", token)
+                const verify = await verifyToken(token)
+                console.log("verify tkn", verify)
+                if (verify?.error === "Invalid token") {
+                    return res.status(501).json({ error: verify.error })
+                }
+                console.log("verify", verify)
+                const { _id } = verify;
+                const { name, email } = await UserModel.findOne({ _id: _id });
+                return res.status(200).json({ name, email, _id });
             } catch (error) {
-                res.status(501).json({ error: error })
+                return res.status(501).json({ error: error })
             }
             break;
         case "POST":
@@ -30,23 +40,26 @@ export default async function UserController(req: NextApiRequest, res: NextApiRe
                 console.log("in login", req?.body)
                 const { email, password } = req?.body;
                 const user = await UserModel.findOne({ email });
-                console.log("before compare call", password, user.password)
+
+                // for comparing hash password
                 const passwordMatch = await comparePassword(password, user.password)
                 console.log("compare password call", passwordMatch)
-                const token = generateToken(user)
-                console.log("tokenns", token)
-                res.status(200).json(user);
+
+                //generate token
+                const token = await generateToken(user)
+                console.log("tokenns", token);
+                return res.status(200).json({ token: token });
             } catch (error) {
-                res.status(501).json({ error: error })
+                return res.status(501).json({ error: error })
             }
             break;
         default:
             try {
                 res.setHeader('Allow Header', ["GET", "POST", "PUT", "DELETE"]);
-                res.status(405).end(`Method ${method} not allowed`);
+                return res.status(405).end(`Method ${method} not allowed`);
             }
             catch (error) {
-                res.status(501).json({ error: error })
+                return res.status(501).json({ error: error })
             }
     }
 }
